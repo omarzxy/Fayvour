6c7186e654ec8887d61dca156d6706a46b4cff5c
Omar Ali,Mohamud <omaralimohamud@omars-macbook-pro.local>
2018-01-09 14:35:41 -0700

    Initial Commit

---
+++ Favour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Favour
+//
+//  Created by Omar Ali,Mohamud on 2018-01-09.
+//  Copyright © 2018 Omar Ali Mohamud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
